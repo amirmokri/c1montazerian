d41d4a7257619483dd3159fbc30213f327800439
amirmokri <amirali.mokri@gmail.com>
2025-02-26 12:08:25 +0330

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -23,6 +6,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+# تنظیمات Swagger و Redoc برای مستندات API
 schema_view = get_schema_view(
     openapi.Info(
         title="Siwan Store API",
@@ -36,13 +20,18 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-
-
-
 urlpatterns = [
+    # مسیرهای مدیریت (Admin Panel)
     path('admin/', admin.site.urls),
-    path('', include('store.urls')),
-    path('api/', include('store.urls')),
+
+    # مسیرهای اصلی اپلیکیشن (Store)
+    path('', include('store.urls')),  # صفحه اصلی و سایر مسیرهای اپلیکیشن Store
+
+    # مسیرهای مستندات API
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# نمایش فایل‌های مدیا هنگام توسعه (Development)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
